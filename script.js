f5ab9b160fb03c3e52dbdadd51ee380be04d15fe
Muhammad Mosa Agha Jahanmal <iamceayber@gmail.com>
2020-11-13 12:56:26 +0430

    added dashboard pages

--- script.js
+++ script.js
@@ -39,3 +39,78 @@ window.onresize = (e) =>
     }
 
 }
+
+const edit_btns = document.querySelectorAll(".edit_btn");
+
+if(edit_btns.length >= 1)
+{
+
+    const form = document.querySelector('form');
+
+    form.onclick = (e) =>
+    {
+        e.target.classList.forEach((value) =>
+        {
+            if(value == "cancel")
+            {
+                clearArea(e.target.parentElement.parentElement.parentElement.querySelector(".d-none"))
+            }
+        })
+    }
+    
+    for (const btn of edit_btns) {
+        btn.onclick = function(e)
+        {
+            e.preventDefault();
+            for (const editbt of edit_btns) {
+                
+                clearArea(editbt)
+            }
+            const parent = btn.parentElement.parentElement;
+            const input = parent.querySelector(".to-top");
+            const title = parent.querySelector(".titles");
+            let bool = true;
+            parent.classList.forEach((value) =>
+            {
+                if(value ==  "alert")
+                {
+                    this.setAttribute('class', 'd-none')
+                    parent.style.height = "200px"
+                    bool = false;
+                    return
+                }
+            })
+            if (bool) {   
+                this.setAttribute('class', 'd-none')
+                title.setAttribute("style", 'display: none')
+                parent.style.height = "110px";
+                input.setAttribute("class", 'to-top account-input-controllers')
+            }
+        }
+    }
+}
+
+function clearArea(editbt) {
+    if(editbt.getAttributeNode("class").value == "d-none")
+    {
+        const parent = editbt.parentElement.parentElement;
+        const input = parent.querySelector(".to-top");
+        const title = parent.querySelector(".titles");
+            editbt.setAttribute('class', 'd-block')
+            parent.style.height = "30px";
+            let bool = true;
+            parent.classList.forEach((value) =>
+            {
+                if(value ==  "alert")
+                {
+                    bool = false;
+                }
+            })
+            if(bool)
+            {
+                input.setAttribute("class", 'to-top')
+                title.setAttribute("style", 'display: block');
+            }
+    }
+    
+}
